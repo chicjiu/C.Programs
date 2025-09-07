ef8fe43e6fb2b982db3c8a97966f09718a191151
SuhaniP <chicjiu1114@gmail.com>
2025-09-07 15:11:33 +0530

    Add files via upload

---
+++ day15code2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, sum = 0, n;
+    printf("Enter a Number: ");
+    scanf("%d", &num);
+
+    while (num > 0)
+    {
+        n = num % 10;
+        sum = (sum * 10) + n;
+        num = num / 10;
+    }
+    printf("%d\n", sum);
+    return 0;
+}
