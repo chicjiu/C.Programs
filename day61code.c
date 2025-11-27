a8abda94c8f8358d217070f9d1c14aaa44c2fd8d
SuhaniP <chicjiu1114@gmail.com>
2025-11-27 22:27:09 +0530

    Add files via upload

---
+++ day61code.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+    int n,k;
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter %d elements: ",n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter the size of k: ");
+    scanf("%d",&k);
+    for(int i=0;i<=n-k;i++)
+    {
+        int found=0;
+        for(int j=i;j<i+k;j++)
+        {
+            if(arr[j]<0)
+            {
+                printf("%d ",arr[j]);
+                found=1;
+                break;
+            }
+        }
+        if(!found)
+        {
+            printf("0 ");
+        }
+    }
+}
