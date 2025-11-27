55edce329000df3affcd8a157451a1262921da6c
SuhaniP <chicjiu1114@gmail.com>
2025-11-27 22:40:34 +0530

    Add files via upload

---
+++ day85code.c
@@ -0,0 +1,24 @@
+//Q135: Assign explicit values starting from 10 and print them.
+
+#include<stdio.h>
+enum status{SUCCESS=1, FAILURE=2, TIMEOUT=3};
+int main(){
+    enum status result;
+    printf("Enter Status: ");
+    scanf("%d",&result);
+    switch (result)
+    {
+        case SUCCESS:
+            printf("Successsful");
+            break;
+        case FAILURE:
+            printf("Failed");
+            break;
+        case TIMEOUT:
+            printf("Timeout");
+            break;
+        default:
+            printf("Invalid.");
+            break;
+    }
+}
